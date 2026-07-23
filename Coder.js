f78ae1fe96eed41a7a7342c0bfb5e65cc6e2e78f
Abhinavjainnit <abhinavjain.nit@gmail.com>
2026-07-23 23:06:59 +0530

    Add files via upload
    About my start of javascript journey and operators practice

---
+++ Coder.js
@@ -0,0 +1,140 @@
+var message='Soham is po'
+console.log(message);
+
+function sayHi(){console.log('Hi')}
+sayHi();
+
+function sayHi(name) {console.log('Hi'+ name)};
+sayHi('Abhibav');
+
+let name='Abhinav'
+console.log('Abhinav')
+
+function multiply(num1,num2){return (num1*num2)} 
+console.log(multiply (2,4));
+
+let legend ={ Name:'Abhiii', Class:'Legend', Home:'Earth'}
+console.log(legend.Class); 
+
+let list=['stack','horse','cow']
+console.log(list[0]);
+
+
+
+let num=10
+num=num+1;
+console.log(num);
+
+let number=143
+number++;
+console.log(number);
+
+let value=65
+value+=1;
+console.log(value);
+
+let vale=69
+vale--;
+console.log(vale); 
+
+let val=27
+++val;
+console.log(val);
+
+let v=794
+v= v+69;
+console.log(v); 
+
+let num1=14;
+let num2=10;
+const isNum1Greater = num1 > num2;
+console.log(isNum1Greater);
+
+let first=186;
+let second=688;
+const firstIsgreaterEqualto= first>second;
+console.log(firstIsgreaterEqualto);
+
+let a=69;
+let b="67";
+const firstIsless= a<b;
+console.log(firstIsless);
+
+let A=2;
+let B=2;
+console.log(A==B);
+console.log(a===b);
+console.log(true==='1');
+
+console.log(1==2);
+
+let age=16;
+const ageDrive= age>= 16? true : false;
+console.log(ageDrive);
+
+let persons=25
+const nobelPrize= persons<=35? true: false;
+console.log(nobelPrize);
+
+let aura=265
+const baddie=aura>500? 'ya': 'na';
+console.log(baddie);
+
+let Kashmir=787
+const Mja=Kashmir<1000? 'Yaa': 'Yay';
+console.log(Mja);
+
+let Abhinav='confused';
+console.log(Abhinav);
+
+let Pawas=['Yoyo','Person'];
+console.log(Pawas[0]);
+
+let Course={Evil:'people'}
+console.log(Course.Evil);
+
+
+let I=100;
+let U=56;
+const iGreaterthanU=I>U;
+console.log(iGreaterthanU);
+
+let d=56;
+let h=67;
+console.log(d-h);
+
+let live=899
+const worthit=live>890? "Yo":"No";
+console.log(worthit);
+
+let six=787
+console.log(six);
+
+
+
+let j=789
+j=j+1;
+console.log(j);
+
+function multiply(num1,num2){ return (num1*num2)} 
+console.log(multiply(1,3));
+
+console.log(true || true);
+console.log (false || true );
+console.log (false || true );
+console.log (false || true); 
+
+let hasReservation= true;
+let acceptingWalkins= false;
+
+const hasAcceptancetoTables= hasReservation|| acceptingWalkins;
+console.log(hasAcceptancetoTables);
+
+function sayHi(Poo){console.log('Hi'+Poo)}
+sayHi('motu'); 
+
+console.log(true&&false);
+
+console.log(b||a);
+
+console.log("life is complicated")
