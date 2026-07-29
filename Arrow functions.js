0f6a741b6cadfeb21363be3c94fa56ca5dd2022c
Abhinavjainnit <abhinavjain.nit@gmail.com>
2026-07-29 22:29:34 +0530

    Add files via upload
    Learned about Arrow function syntax in javascript.

---
+++ Arrow functions.js
@@ -0,0 +1,6 @@
+// function add(num1, num2) { return num1+ num2 } 
+// Arrow functions dp the same jpb
+
+const add = (num1, num2)=> { return num1+ num2 } 
+// functions are curly bracket objects in javascript
+// if there is only one function you xan remove curly braces and also the return keyword
